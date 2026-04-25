afde9e7102d69a4267a3efa59feeebd23b1ae985
Adi7450 <adityaraj9864@gmail.com>
2026-04-26 01:11:35 +0530

    Changing node.config.ts to node.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,15 @@
-import type { NextConfig } from "next";
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
       {
+        // This redirects any call to /api/... to your Python backend
         source: "/api/:path*",
         destination: "http://localhost:8000/api/:path*",
       },
     ];
   },
 };
+
 export default nextConfig;
